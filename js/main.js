d232511c9c235ec04075fc6ff07adba3bcf5e060
mikemouse <kmmyemail@ukr.net>
2023-03-22 14:00:37 +0200

    feat: add calories calculator v1

--- js/main.js
+++ js/main.js
@@ -461,4 +461,87 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  //CALORIE CALCULATOR
+  //1. Both sex and activity has the same html nasting so use function to handle data from them
+  //2. HTML add sex id male female
+  //3. HTML add activity data-ratio 1.2 1.375 1.55 1.725
+  //4. query result span
+  //5. let sex, height, weight, age, ratio
+  //6. fn calcTotal check falsy^ yes result.textContent = 'Fill all fields please'; return;
+  //7. if sex = female result.textContent = formula female (link inside) else male
+  //8. getStaticInfo(parentSelector, activeClass) query parent addEvent if e.target data-ratio ratio = e.target data-ratio else sex = get id all active remove add current
+
+  const result = document.querySelector('.calculating__result span');
+  let sex = 'female',
+    height,
+    weight,
+    age,
+    ratio = 1.375;
+
+  function calcTotal() {
+    if (!sex || !height || !weight || !age || !ratio) {
+      result.textContent = '____';
+      return;
+    }
+
+    if (sex === 'female') {
+      result.textContent = Math.round(
+        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
+      );
+    } else {
+      result.textContent = Math.round(
+        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
+      );
+    }
+  }
+
+  calcTotal();
+
+  function getStaticInfo(parentSelector, activeClass) {
+    const elements = document.querySelectorAll(`${parentSelector} div`);
+
+    elements.forEach((elem) => {
+      elem.addEventListener('click', (e) => {
+        if (e.target.getAttribute('data-ratio')) {
+          ratio = +e.target.getAttribute('data-ratio');
+        } else {
+          sex = e.target.getAttribute('id');
+        }
+
+        elements.forEach((elem) => {
+          elem.classList.remove(activeClass);
+        });
+
+        e.target.classList.add(activeClass);
+        calcTotal();
+      });
+    });
+  }
+
+  getStaticInfo('#gender', 'calculating__choose-item_active');
+  getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active');
+
+  function getDynamicInfo(selector) {
+    const input = document.querySelector(selector);
+
+    input.addEventListener('input', () => {
+      switch (input.getAttribute('id')) {
+        case 'height':
+          height = +input.value;
+          break;
+        case 'weight':
+          weight = +input.value;
+          break;
+        case 'age':
+          age = +input.value;
+          break;
+      }
+      calcTotal();
+    });
+  }
+
+  getDynamicInfo('#height');
+  getDynamicInfo('#weight');
+  getDynamicInfo('#age');
 });
